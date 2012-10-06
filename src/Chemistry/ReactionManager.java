9410efbf0ab27be1adeed0ecc4d6401be1025e26
Lee Chang Yoon <cyoon47@hotmail.com>
2012-10-06 18:21:33 +0800

    ReactionManager updated with getConc and calcReactionQuotient

--- src/Chemistry/ReactionManager.java
+++ src/Chemistry/ReactionManager.java
@@ -15,17 +15,34 @@ public class ReactionManager {
 	public void update () {
 		
 	}
-	
+	*/
+        
 	public float getConcA () {
-		
+            int numA = 0;
+            for(Molecule m: pEngine.getParticles()){
+                if(m.getType() == Molecule.MOLECULE_A) numA++;
+            }
+            return (float) (numA / 1000.0);
 	}
 	
 	public float getConcB () {
-		
+            int numB = 0;
+            for(Molecule m: pEngine.getParticles()){
+                if(m.getType() == Molecule.MOLECULE_B) numB++;
+            }
+            return (float) (numB / 1000.0);
 	}
+         
+        public float getConcC (){
+            int numC = 0;
+            for(Molecule m: pEngine.getParticles()){
+                if(m.getType() == Molecule.MOLECULE_C) numC++;
+            }
+            return (float) (numC / 1000.0);
+        }
 	
 	public float calcReactionQuotient () {
-		
-	}*/
+            return (getConcC() * getConcC() / (getConcB() * getConcA()));
+	}
 }
 
